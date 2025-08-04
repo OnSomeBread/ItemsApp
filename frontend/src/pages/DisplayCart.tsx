6b6af71d51e943661971e61013318b3d4d7beb4c
OnSomeBread <mark1qaz@comcast.net>
2025-08-04 10:28:35 -0500

    added +money animation when adding items to cart

--- frontend/src/pages/DisplayCart.tsx
+++ frontend/src/pages/DisplayCart.tsx
@@ -4,8 +4,29 @@ import { useEffect, useState } from "react";
 import ItemComponent from "../components/ItemComponent";
 import { AnimatePresence, motion } from "framer-motion";
 
+const getTotalFleaPrice = (allItems: Item[]) => {
+  const prevTotal = parseInt(localStorage.getItem("prevFleaMarket") || "0");
+  let totalFleaPrice = 0;
+
+  allItems.forEach((item) => {
+    for (const sell of item.sells) {
+      if (sell.source === "fleaMarket") {
+        totalFleaPrice +=
+          sell.price *
+          parseInt(localStorage.getItem("item-" + item._id) || "0");
+        break;
+      }
+    }
+  });
+
+  localStorage.setItem("prevFleaMarket", totalFleaPrice.toString());
+  return [totalFleaPrice, prevTotal];
+};
+
 function DisplayCart() {
   const [allItems, setAllItems] = useState<Item[] | null>(null);
+  const [currFleaPrice, setCurrFleaPrice] = useState<number>(0);
+  const [priceDelta, setPriceDelta] = useState<number | null>(null);
 
   const params = new URLSearchParams();
   const keys = Object.keys(localStorage);
@@ -20,45 +41,40 @@ function DisplayCart() {
     axios
       .get<Item[]>(query)
       .then((response) => {
-        console.log(response.data);
         response.data.sort((itema, itemb) =>
           itema._id.localeCompare(itemb._id)
         );
         setAllItems(response.data);
+        const [currPrice, prevPrice] = getTotalFleaPrice(response.data);
+        setCurrFleaPrice(currPrice);
+
+        if (currPrice > prevPrice) {
+          setPriceDelta(currPrice - prevPrice);
+          //setTimeout(() => setPriceDelta(null), 1000);
+        }
       })
       .catch((err) => console.log(err));
   }, [query]);
-
-  const getTotalFleaPrice = () => {
-    let totalFleaPrice = 0;
-    //const prevTotal = parseInt(localStorage.getItem("prevFleaMarket") || "0");
-    allItems?.forEach((item) => {
-      for (const sell of item.sells) {
-        if (sell.source === "fleaMarket") {
-          totalFleaPrice +=
-            sell.price *
-            parseInt(localStorage.getItem("item-" + item._id) || "0");
-          break;
-        }
-      }
-    });
-    localStorage.setItem("FleaMarketDiff", totalFleaPrice.toString());
-    return totalFleaPrice;
-  };
 
   return (
     <>
       <p>Total Flea Market Price</p>
       <div className="div-align">
-        <p>{getTotalFleaPrice().toLocaleString("en-us")} RUB</p>
-        {/* TODO ADD A +AMOUNT ANIMATION
+        <p>{currFleaPrice.toLocaleString("en-us")} RUB</p>
         <motion.p
+          key={priceDelta}
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
+          animate={{
+            opacity: [0, 1, 1, 0],
+          }}
+          transition={{
+            duration: 1.6,
+            times: [0, 0.1, 0.9, 1],
+          }}
+          style={{ color: "green" }}
         >
-          {0}
-        </motion.p> */}
+          +{priceDelta?.toLocaleString("en-us")}
+        </motion.p>
       </div>
 
       <motion.div
@@ -70,17 +86,13 @@ function DisplayCart() {
         <AnimatePresence>
           {allItems?.map((x, i) => (
             <motion.li
+              key={x._id + i.toString()}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ duration: 0.7 }}
+              transition={{ duration: 0.7, ease: "easeOut" }}
             >
-              <ItemComponent
-                key={x._id + i.toString()}
-                item={x}
-                idx={i}
-                fields={["name", "fleaMarket"]}
-              >
+              <ItemComponent item={x} idx={i} fields={["name", "fleaMarket"]}>
                 <p>count: {localStorage.getItem("item-" + x._id)}</p>
               </ItemComponent>
             </motion.li>
