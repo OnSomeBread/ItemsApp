42d8afaf9d3536e8a3fdd075a12eb909f2b331b4
OnSomeBread <mark1qaz@comcast.net>
2025-08-13 13:48:29 -0500

    added addition information on the itemView page

--- frontend/src/components/ItemChartComponent.tsx
+++ frontend/src/components/ItemChartComponent.tsx
@@ -15,14 +15,24 @@ interface Props {
 
 function ItemChartComponent({ itemHistory }: Props) {
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <LineChart data={itemHistory}>
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart
+        data={itemHistory}
+        margin={{ top: 0, right: 40, left: 10, bottom: 0 }}
+      >
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis
           dataKey="time"
           tickFormatter={(t) => new Date(t).toLocaleTimeString()}
         />
-        <YAxis />
+        <YAxis
+          tickFormatter={(value) =>
+            new Intl.NumberFormat("en-US", {
+              notation: "compact",
+              compactDisplay: "short",
+            }).format(value)
+          }
+        />
         <Tooltip labelFormatter={(t) => new Date(t).toLocaleTimeString()} />
         <Line
           type="monotone"
--- frontend/src/pages/ItemView.tsx
+++ frontend/src/pages/ItemView.tsx
@@ -16,23 +16,52 @@ function ItemView() {
         BACKEND_ADDRESS + "/api/item_history?item_id=" + item._id
       )
       .then((response) => {
-        if (response.data === undefined) return;
-        if (response.data.length > 0) {
-          const itemHistoryArr = response.data.map((d) => ({
-            ...d,
+        if (response.data === undefined || response.data.length === 0) return;
 
-            time: new Date(`2025-08-01T${d.time}Z`),
-          }));
+        // set all of the dates to a proper format using junkdate since it wont
+        // be used in the graph
+        const itemHistoryArr = response.data.map((d) => ({
+          ...d,
 
-          console.log(itemHistoryArr[0].time);
+          time: new Date(`2025-01-01T${d.time}Z`),
+        }));
 
-          setItemHistory(itemHistoryArr);
-        }
+        setItemHistory(itemHistoryArr);
       });
   }, [item._id]);
 
   return (
-    <div>
+    <div style={{ padding: 30 }}>
+      <p>{item.name}</p>
+      <p>item short name: {item.shortName}</p>
+      {item.types && (
+        <p>item types: {item.types.map((t) => t.name).join(", ")}</p>
+      )}
+      <p>item average 24 hour price: {item.avg24hPrice}</p>
+      <p>item base price: {item.basePrice}</p>
+      <p>change last 48 hours: {item.changeLast48hPercent}%</p>
+      <p>item width: {item.width}</p>
+      <p>item height: {item.height}</p>
+      <a href={item.link}>
+        <p>{item.name} wiki page</p>
+      </a>
+
+      {item.sells && (
+        <p>
+          Sell Prices:{" "}
+          {item.sells
+            .map((sellFor) => sellFor.source + ": " + sellFor.price.toString())
+            .join(", ")}
+        </p>
+      )}
+
+      <p>Item Price History Chart</p>
+      <p>
+        <mark>
+          WARNING CURRENT WIPE DOES NOT HAVE FLEA SO IT ALWAYS DISPLAYS AT A
+          CONSTANT PRICE
+        </mark>
+      </p>
       <ItemChartComponent itemHistory={itemHistory ?? []} />
     </div>
   );
