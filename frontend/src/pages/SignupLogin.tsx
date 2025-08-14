e736d8f5768a7ab21157c9a5f5e14dff426a30e0
OnSomeBread <mark1qaz@comcast.net>
2025-08-14 11:05:05 -0500

    added more test endpoints for token

--- frontend/src/pages/SignupLogin.tsx
+++ frontend/src/pages/SignupLogin.tsx
@@ -1,7 +1,8 @@
 import api from "../api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function SignupLogin() {
+  const [loggedIn, setLoggedIn] = useState(false);
   useEffect(() => {
     api
       .post("/token/login", {
@@ -11,14 +12,39 @@ function SignupLogin() {
       .then((response) => {
         console.log(response.data);
         localStorage.setItem("access_token", response.data.access_token);
+        setLoggedIn(true);
       });
   }, []);
 
   useEffect(() => {
-    api.get("/token/me").then((response) => {
-      console.log(response.data);
-    });
-  }, []);
+    if (!loggedIn) return;
+    api
+      .get("/token/me")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, [loggedIn]);
+
+  useEffect(() => {
+    if (!loggedIn) return;
+    api
+      .get("/token/pref_tasks")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, [loggedIn]);
+
+  useEffect(() => {
+    if (!loggedIn) return;
+    api
+      .get("/token/pref_items")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, [loggedIn]);
 
   return <></>;
 }
