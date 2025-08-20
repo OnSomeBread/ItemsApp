18abac215d36c89cdc29973622a6e99a2af28d48
OnSomeBread <mark1qaz@comcast.net>
2025-08-20 14:51:01 -0500

    removed extra comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import NotFound from "./pages/NotFound";
-// import Home from "./pages/Home";
-// import ProtectedRoute from "./components/ProtectedRoute";
 import DisplayItems from "./pages/DisplayItems";
 import DisplayTasks from "./pages/DisplayTasks";
 import SignupLogin from "./pages/SignupLogin";
@@ -11,18 +6,7 @@ import TaskView from "./pages/TaskView";
 import ItemView from "./pages/ItemView";
 import TaskTree from "./pages/TaskTree";
 
-// function LogOut() {
-//   localStorage.clear();
-//   return <Navigate to="/login" />;
-// }
-
-// function RegisterAndLogout() {
-//   localStorage.clear();
-//   return <Register />;
-// }
-
 function App() {
-  // only the first route will be used for now before all of the jwt tokens are finished
   return (
     <BrowserRouter>
       <Routes>
@@ -32,15 +16,6 @@ function App() {
         <Route path="/item_view" element={<ItemView />} />
         <Route path="/signup" element={<SignupLogin />} />
         <Route path="/task_tree" element={<TaskTree />} />
-        {/* <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        ></Route>
-        <Route path="/login" element={<Login />} /> */}
       </Routes>
     </BrowserRouter>
   );
