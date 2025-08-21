78664b0350ed6629f74c1efe51ac6621b9ec3020
OnSomeBread <mark1qaz@comcast.net>
2025-08-21 09:15:34 -0500

    removed JWT algo from environment file

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -7,7 +7,7 @@ import jwt
 import os 
 
 JWT_SECRET = os.environ['SECRET_KEY']
-JWT_ALGORITHM = os.environ['ALGORITHM']
+JWT_ALGORITHM = "HS256"
 ACCESS_EXPIRE_MINUTES = 30
 REFRESH_EXPIRE_DAYS = 7
 
