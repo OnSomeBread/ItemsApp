bdf96cf484ea040675d430fb0eef6b3aa9f7ba01
OnSomeBread <mark1qaz@comcast.net>
2025-08-23 17:56:16 -0500

    added health check

--- backend/api/fastapi_views.py
+++ backend/api/fastapi_views.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 from django.core.asgi import get_asgi_application
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from asgiref.sync import sync_to_async
 from fastapi.middleware.cors import CORSMiddleware
 from .api_scheduler import lifespan
 
@@ -27,3 +28,13 @@ app.include_router(token.router)
 app.include_router(items.router)
 app.include_router(tasks.router)
 app.include_router(pastApi.router)
+
+@app.get('/health')
+async def health():
+    from api.models import Task, Item
+    try:
+        if await sync_to_async(Item.objects.count)() == 0 or await sync_to_async(Task.objects.count)() == 0:
+            raise 'data base has not been initialized'
+        return {"status":"ok"}
+    except Exception as e:
+        raise HTTPException(status_code=503, detail=str(e))
