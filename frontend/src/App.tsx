976dc10146371327a2650239a2902e550dbfa1ad
OnSomeBread <mark1qaz@comcast.net>
2025-08-25 18:57:25 -0500

    disabled signup route since it is not fully setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DisplayItems from "./pages/DisplayItems";
 import DisplayTasks from "./pages/DisplayTasks";
-import SignupLogin from "./pages/SignupLogin";
 import TaskView from "./pages/TaskView";
 import ItemView from "./pages/ItemView";
 import TaskTree from "./pages/TaskTree";
 import "./App.css";
-import PageSwitch from "./components/PageSwitch";
+// import SignupLogin from "./pages/SignupLogin";
+// import PageSwitch from "./components/PageSwitch";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<PageSwitch />} />
+        <Route path="*" element={<DisplayItems />} />
         <Route path="/items" element={<DisplayItems />} />
         <Route path="/tasks" element={<DisplayTasks />} />
         <Route path="/task_view" element={<TaskView />} />
         <Route path="/item_view" element={<ItemView />} />
-        <Route path="/signup" element={<SignupLogin />} />
         <Route path="/task_tree" element={<TaskTree />} />
+        {/* <Route path="/signup" element={<SignupLogin />} /> */}
       </Routes>
     </BrowserRouter>
   );
