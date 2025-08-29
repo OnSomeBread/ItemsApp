0ee1e88b14743a0e67179e9b01cc59c7e00aec59
OnSomeBread <mark1qaz@comcast.net>
2025-08-29 11:47:06 -0500

    made page switch buttons instead inside of nav elem

--- frontend/src/components/PageSwitch.tsx
+++ frontend/src/components/PageSwitch.tsx
@@ -2,12 +2,32 @@ import { useNavigate } from "react-router-dom";
 
 function PageSwitch() {
   const navigate = useNavigate();
+
+  const buttonClass = "outline contrast main-btn search-btn";
+
   return (
-    <div style={{ display: "flex", gap: "20px" }}>
-      <a onClick={() => navigate("/items")}>Items page</a>
-      <a onClick={() => navigate("/tasks")}>Tasks page</a>
-      <a onClick={() => navigate("/task_tree")}>Task Tree page</a>
-    </div>
+    <nav
+      style={{
+        justifyContent: "left",
+        gap: "20px",
+        marginTop: 12,
+        marginLeft: 18,
+        marginBottom: 4,
+      }}
+    >
+      <button className={buttonClass} onClick={() => navigate("/items")}>
+        Items page
+      </button>
+      <button className={buttonClass} onClick={() => navigate("/tasks")}>
+        Tasks page
+      </button>
+      <button className={buttonClass} onClick={() => navigate("/task_tree")}>
+        Task Tree page
+      </button>
+      <button className={buttonClass} onClick={() => window.history.back()}>
+        Go Back
+      </button>
+    </nav>
   );
 }
 
