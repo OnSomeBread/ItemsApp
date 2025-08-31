21a79e73dbc3266fb92d032dc6e1f8fed2365155
Mark Schaefer <markschaefer@Marks-MacBook-Air.local>
2025-08-31 14:22:21 -0500

    always refresh only in prod or when empty

--- backend/api/api_scheduler.py
+++ backend/api/api_scheduler.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from django.core.management import call_command
 from asgiref.sync import sync_to_async
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from api.settings import DEBUG
 
 scheduler = AsyncIOScheduler()
 
@@ -16,8 +17,10 @@ async def lifespan(app: FastAPI):
     await sync_to_async(call_command)('makemigrations', 'api')
     await sync_to_async(call_command)('migrate', 'api')
 
-    create_task(sync_to_async(call_command)('upsert_file', 'items', 'most_recent_items.json'))
-    create_task(sync_to_async(call_command)('upsert_file', 'tasks', 'most_recent_tasks.json'))
+    if not DEBUG or await sync_to_async(Item.objects.count)() == 0:
+        create_task(sync_to_async(call_command)('upsert_file', 'items', 'most_recent_items.json'))
+    if not DEBUG or await sync_to_async(Task.objects.count)() == 0:
+        create_task(sync_to_async(call_command)('upsert_file', 'tasks', 'most_recent_tasks.json'))
 
     # TODO replace file upserts with api ones only in production
     # call_command('upsert_items_api')
