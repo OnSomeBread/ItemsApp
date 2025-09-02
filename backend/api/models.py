1e5f8814a3b330c65742c598e28177c0a2ed62c6
Mark Schaefer <markschaefer@Marks-MacBook-Air.local>
2025-09-02 09:48:50 -0500

    shortened name of django models to help with switching orms

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,91 +1,91 @@
-from django.db import models
+from django.db.models import *
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # every item can have many types
-class ItemTypes(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+class ItemTypes(Model):
+    name = CharField(max_length=50, unique=True)
 
-class Item(models.Model):
-    _id = models.CharField(max_length=24, primary_key=True, db_index=True)
-    name = models.CharField(max_length=100, db_index=True)
-    shortName = models.CharField(max_length=50)
+class Item(Model):
+    _id = CharField(max_length=24, primary_key=True, db_index=True)
+    name = CharField(max_length=100, db_index=True)
+    shortName = CharField(max_length=50)
 
-    avg24hPrice = models.IntegerField(default=0, null=True)
-    basePrice = models.IntegerField(default=0)
-    changeLast48hPercent = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    avg24hPrice = IntegerField(default=0, null=True)
+    basePrice = IntegerField(default=0)
+    changeLast48hPercent = DecimalField(max_digits=8, decimal_places=2, null=True)
 
-    width = models.IntegerField(default=0)
-    height = models.IntegerField(default=0)
-    link = models.URLField()
+    width = IntegerField(default=0)
+    height = IntegerField(default=0)
+    link = URLField()
 
-    itemtypes = models.ManyToManyField(ItemTypes, blank=True)
+    itemtypes = ManyToManyField(ItemTypes, blank=True)
 
 # every item can have many sources to sell from for different prices
-class SellFor(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='sells')
-    price = models.IntegerField(default=0)
-    source = models.CharField(max_length=50)
+class SellFor(Model):
+    item = ForeignKey(Item, on_delete=CASCADE, related_name='sells')
+    price = IntegerField(default=0)
+    source = CharField(max_length=50)
 
 # since api calls are small this model stores only the data that changes often for each item
-class PastApiCalls(models.Model):
-    api_name = models.CharField(max_length=50)
-    time = models.TimeField()
+class PastApiCalls(Model):
+    api_name = CharField(max_length=50)
+    time = TimeField()
 
-class SavedItemData(models.Model):
-    past_api_call = models.ForeignKey(PastApiCalls, on_delete=models.CASCADE, related_name='past_items')
+class SavedItemData(Model):
+    past_api_call = ForeignKey(PastApiCalls, on_delete=CASCADE, related_name='past_items')
 
     # this should match up exactly with the related Item class id
     # could instead use reference with item but not needed here
-    item_id = models.CharField(max_length=24, db_index=True)
+    item_id = CharField(max_length=24, db_index=True)
 
-    avg24hPrice = models.IntegerField(default=0, null=True)
-    changeLast48hPercent = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    fleaMarket = models.IntegerField(default=0)
+    avg24hPrice = IntegerField(default=0, null=True)
+    changeLast48hPercent = DecimalField(max_digits=8, decimal_places=2, null=True)
+    fleaMarket = IntegerField(default=0)
 
 # saving task data does not need to happen not nearly as often as saving item data
 # since its not actively changing but also all of the data needs to be saved in order to restore
-class SavedTaskData(models.Model):
-    past_api_call = models.ForeignKey(PastApiCalls, on_delete=models.CASCADE, related_name='past_tasks')
-    task_data = models.JSONField(default=dict)
+class SavedTaskData(Model):
+    past_api_call = ForeignKey(PastApiCalls, on_delete=CASCADE, related_name='past_tasks')
+    task_data = JSONField(default=dict)
 
-class Task(models.Model):
-    _id = models.CharField(max_length=24, primary_key=True, db_index=True)
-    name = models.CharField(max_length=100, db_index=True)
-    normalizedName = models.CharField(max_length=100, null=True)
+class Task(Model):
+    _id = CharField(max_length=24, primary_key=True, db_index=True)
+    name = CharField(max_length=100, db_index=True)
+    normalizedName = CharField(max_length=100, null=True)
 
     # there is another chart that maps experience points to player level
     # might remove experience here as it might just always represent minPlayerLevel
-    experience = models.IntegerField(default=0, null=True)
-    minPlayerLevel = models.IntegerField(default=0)
+    experience = IntegerField(default=0, null=True)
+    minPlayerLevel = IntegerField(default=0)
 
     # task giver
-    trader = models.CharField(max_length=100, null=True)
-    factionName = models.CharField(max_length=100)
-    kappaRequired = models.BooleanField(default=False)
-    lightkeeperRequired = models.BooleanField(default=False)
-    wiki = models.URLField() # technically called wikiLink
+    trader = CharField(max_length=100, null=True)
+    factionName = CharField(max_length=100)
+    kappaRequired = BooleanField(default=False)
+    lightkeeperRequired = BooleanField(default=False)
+    wiki = URLField() # technically called wikiLink
 
 # each task can have multiple task requirements which are other Task objects but here just grab id
 # since that is all that is needed to make an adjancency list (Task Graph)
-class TaskRequirement(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='taskRequirements')
-    status = models.CharField(max_length=100)
-    reqTaskId = models.CharField(max_length=24, db_index=True)
+class TaskRequirement(Model):
+    task = ForeignKey(Task, on_delete=CASCADE, related_name='taskRequirements')
+    status = CharField(max_length=100)
+    reqTaskId = CharField(max_length=24, db_index=True)
 
-class Map(models.Model):
-    _id = models.CharField(max_length=24, primary_key=True, db_index=True)
-    name = models.CharField(max_length=100, db_index=True)
-    normalizedName = models.CharField(max_length=100)
-    players = models.CharField(max_length=100)
-    description = models.TextField()
-    wiki = models.URLField()
+class Map(Model):
+    _id = CharField(max_length=24, primary_key=True, db_index=True)
+    name = CharField(max_length=100, db_index=True)
+    normalizedName = CharField(max_length=100)
+    players = CharField(max_length=100)
+    description = TextField()
+    wiki = URLField()
 
-class Objective(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='objectives')
-    _id = models.CharField(max_length=24, primary_key=True, db_index=True)
-    objType = models.CharField(max_length=100)
-    description = models.TextField()
-    maps = models.ManyToManyField(Map, blank=True)
+class Objective(Model):
+    task = ForeignKey(Task, on_delete=CASCADE, related_name='objectives')
+    _id = CharField(max_length=24, primary_key=True, db_index=True)
+    objType = CharField(max_length=100)
+    description = TextField()
+    maps = ManyToManyField(Map, blank=True)
 
 
 class UserManager(BaseUserManager):
@@ -97,14 +97,14 @@ class UserManager(BaseUserManager):
 
 # auth users model
 class User(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    # is_active = models.BooleanField(default=True)
-    # is_staff = models.BooleanField(default=False)
-    # is_superuser = models.BooleanField(default=False)
+    email = EmailField(unique=True)
+    # is_active = BooleanField(default=True)
+    # is_staff = BooleanField(default=False)
+    # is_superuser = BooleanField(default=False)
 
     # this stores the users site interations
-    preferences_tasks = models.JSONField(default=dict, null=True)
-    preferences_items = models.JSONField(default=dict, null=True)
+    preferences_tasks = JSONField(default=dict, null=True)
+    preferences_items = JSONField(default=dict, null=True)
 
     objects = UserManager()
 
