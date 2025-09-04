8e3bd70cb730c425159fb27d7a44e782d3626346
OnSomeBread <mark1qaz@comcast.net>
2025-09-04 13:12:31 -0500

    lazy load entire app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DisplayItems from "./pages/DisplayItems";
-import DisplayTasks from "./pages/DisplayTasks";
-import TaskView from "./pages/TaskView";
-import ItemView from "./pages/ItemView";
-import TaskTree from "./pages/TaskTree";
+import { lazy, Suspense } from "react";
 import "./App.css";
 // import SignupLogin from "./pages/SignupLogin";
 // import PageSwitch from "./components/PageSwitch";
 
+const DisplayItems = lazy(() => import("./pages/DisplayItems"));
+const DisplayTasks = lazy(() => import("./pages/DisplayTasks"));
+const TaskView = lazy(() => import("./pages/TaskView"));
+const ItemView = lazy(() => import("./pages/ItemView"));
+const TaskTree = lazy(() => import("./pages/TaskTree"));
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<DisplayItems />} />
-        <Route path="/items" element={<DisplayItems />} />
-        <Route path="/tasks" element={<DisplayTasks />} />
-        <Route path="/task_view" element={<TaskView />} />
-        <Route path="/item_view" element={<ItemView />} />
-        <Route path="/task_tree" element={<TaskTree />} />
-        {/* <Route path="/signup" element={<SignupLogin />} /> */}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="*" element={<DisplayItems />} />
+          <Route path="/items" element={<DisplayItems />} />
+          <Route path="/tasks" element={<DisplayTasks />} />
+          <Route path="/task_view" element={<TaskView />} />
+          <Route path="/item_view" element={<ItemView />} />
+          <Route path="/task_tree" element={<TaskTree />} />
+          {/* <Route path="/signup" element={<SignupLogin />} /> */}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
