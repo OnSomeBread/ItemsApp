b9828e90d85742820993b36fb9bf454153793c13
OnSomeBread <mark1qaz@comcast.net>
2025-09-17 15:56:18 -0500

    changed inserts item and task inserts to bulk inserts for much faster performance

--- backend/src/upsert.rs
+++ backend/src/upsert.rs
@@ -149,85 +149,158 @@ async fn upsert_items(
     let mut txn = pool.begin().await?;
 
     let ids: Vec<String> = items.iter().map(|item| item._id.to_string()).collect();
-    let names: Vec<String> = items
-        .iter()
-        .map(|item| item.item_name.to_string())
-        .collect();
-    let short_names: Vec<String> = items
-        .iter()
-        .map(|item| item.short_name.to_string())
-        .collect();
-    let avg_24h_prices: Vec<i32> = items
-        .iter()
-        .map(|item| item.avg_24h_price.unwrap_or(0))
-        .collect();
-    let base_prices: Vec<i32> = items.iter().map(|item| item.base_price).collect();
-    let change_last_48h_percents: Vec<f32> = items
-        .iter()
-        .map(|item| item.change_last_48h_percent.unwrap_or(0.0))
-        .collect();
-    let widths: Vec<i32> = items.iter().map(|item| item.width).collect();
-    let heights: Vec<i32> = items.iter().map(|item| item.height).collect();
-    let wikis: Vec<String> = items.iter().map(|item| item.wiki.to_string()).collect();
-    let types_arr: Vec<String> = items
-        .iter()
-        .map(|item| item.item_types.join(", "))
-        .collect();
+    let names: Vec<String> = items.iter().map(|x| x.item_name.to_string()).collect();
+    let short_names: Vec<String> = items.iter().map(|x| x.short_name.to_string()).collect();
+    let avg_24h_prices: Vec<i32> = items.iter().map(|x| x.avg_24h_price.unwrap_or(0)).collect();
+    let change_last_48h_percents: Vec<f32> = items.iter().map(|x| x.change_last_48h_percent.unwrap_or(0.0)).collect();
+    let types_arr: Vec<String> = items.iter().map(|x| x.item_types.join(", ")).collect();
 
+    // ITEM BULK INSERT
     sqlx::query!("INSERT INTO Item (_id, item_name, short_name, avg_24h_price, base_price, change_last_48h_percent, width, height, wiki, item_types) SELECT * FROM UNNEST($1::text[], $2::text[], $3::text[], $4::int[], $5::int[], $6::real[], $7::int[], $8::int[], $9::text[], $10::text[]) ON CONFLICT(_id) DO UPDATE SET avg_24h_price = EXCLUDED.avg_24h_price, change_last_48h_percent = EXCLUDED.change_last_48h_percent;",
         &ids,
         &names,
         &short_names,
         &avg_24h_prices,
-        &base_prices,
+        &items.iter().map(|item| item.base_price).collect::<Vec<i32>>(),
         &change_last_48h_percents,
-        &widths,
-        &heights,
-        &wikis,
+        &items.iter().map(|item| item.width).collect::<Vec<i32>>(),
+        &items.iter().map(|item| item.height).collect::<Vec<i32>>(),
+        &items.iter().map(|item| item.wiki.to_string()).collect::<Vec<String>>(),
         &types_arr)
         .execute(&mut *txn)
         .await?;
 
-    // let buy_for_arr: Vec<Vec<BuyFor>> = items.iter().map(|item| item.buys.clone()).collect();
-    // .iter()
-    // .map(|buy| buy.price)
-    // .collect();
+    let buy_for_prices: Vec<i32> = items
+        .iter()
+        .flat_map(|x| x.buys.iter().map(|y| y.price).collect::<Vec<i32>>())
+        .collect();
+    let buy_for_currencys: Vec<String> = items
+        .iter()
+        .flat_map(|x| {
+            x.buys
+                .iter()
+                .map(|y| y.currency.to_string())
+                .collect::<Vec<String>>()
+        })
+        .collect();
+    let buy_for_price_rubs: Vec<i32> = items
+        .iter()
+        .flat_map(|x| x.buys.iter().map(|y| y.price_rub).collect::<Vec<i32>>())
+        .collect();
+    let buy_for_traders: Vec<String> = items
+        .iter()
+        .flat_map(|x| {
+            x.buys
+                .iter()
+                .map(|y| y.vendor.trader_name.to_string())
+                .collect::<Vec<String>>()
+        })
+        .collect();
+    let buy_for_min_trader_levels: Vec<i32> = items
+        .iter()
+        .flat_map(|x| {
+            x.buys
+                .iter()
+                .map(|y| y.vendor.min_trader_level)
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+    let buy_for_buy_limit: Vec<i32> = items
+        .iter()
+        .flat_map(|x| {
+            x.buys
+                .iter()
+                .map(|y| y.vendor.buy_limit)
+                .collect::<Vec<i32>>()
+        })
+        .collect();
 
-    for item in items {
-        for buy in item.buys.iter() {
-            sqlx::query!(
-                "INSERT INTO BuyFor (price, currency, price_rub, trader_name, min_trader_level, buy_limit, item_id) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (id) DO UPDATE SET price = EXCLUDED.price, currency = EXCLUDED.currency, price_rub = EXCLUDED.price_rub, trader_name = EXCLUDED.trader_name, min_trader_level = EXCLUDED.min_trader_level, buy_limit = EXCLUDED.buy_limit, item_id = EXCLUDED.item_id;",
-                &buy.price,
-                &buy.currency,
-                &buy.price_rub,
-                &buy.vendor.trader_name,
-                &buy.vendor.min_trader_level,
-                &buy.vendor.buy_limit,
-                &item._id,
-            ).execute(&mut *txn)
-            .await?;
-        }
+    let buy_for_item_ids = items.iter().flat_map(|x| vec![x._id.to_string(); x.buys.len()]).collect::<Vec<String>>();
 
-        for sell in item.sells.iter() {
-            sqlx::query!(
-                "INSERT INTO SellFor (price, currency, price_rub, trader_name, sell_offer_fee_rate, sell_requirement_fee_rate, found_in_raid_required, item_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT (id) DO UPDATE SET price = EXCLUDED.price, currency = EXCLUDED.currency, price_rub = EXCLUDED.price_rub, trader_name = EXCLUDED.trader_name, sell_offer_fee_rate = EXCLUDED.sell_offer_fee_rate, sell_requirement_fee_rate = EXCLUDED.sell_requirement_fee_rate, found_in_raid_required = EXCLUDED.found_in_raid_required, item_id = EXCLUDED.item_id;",
-                &sell.price,
-                &sell.currency,
-                &sell.price_rub,
-                &sell.vendor.trader_name,
-                &sell.vendor.sell_offer_fee_rate.unwrap_or(0),
-                &sell.vendor.sell_requirement_fee_rate.unwrap_or(0),
-                &sell.vendor.found_in_raid_required.unwrap_or(false),
-                &item._id,
-            ).execute(&mut *txn)
-            .await?;
-        }
-    }
+    // BUYFOR BULK INSERT
+    sqlx::query!("INSERT INTO BuyFor (price, currency, price_rub, trader_name, min_trader_level, buy_limit, item_id) SELECT * FROM UNNEST($1::int[], $2::text[], $3::int[], $4::text[], $5::int[], $6::int[], $7::text[]) ON CONFLICT (id) DO UPDATE SET price = EXCLUDED.price, currency = EXCLUDED.currency, price_rub = EXCLUDED.price_rub, trader_name = EXCLUDED.trader_name, min_trader_level = EXCLUDED.min_trader_level, buy_limit = EXCLUDED.buy_limit, item_id = EXCLUDED.item_id;",
+        &buy_for_prices,
+        &buy_for_currencys,
+        &buy_for_price_rubs,
+        &buy_for_traders,
+        &buy_for_min_trader_levels,
+        &buy_for_buy_limit,
+        &buy_for_item_ids).execute(&mut *txn).await?;
+
+    let sell_for_prices: Vec<i32> = items
+        .iter()
+        .flat_map(|x| x.sells.iter().map(|y| y.price).collect::<Vec<i32>>())
+        .collect();
+    let sell_for_currencys: Vec<String> = items
+        .iter()
+        .flat_map(|x| {
+            x.sells
+                .iter()
+                .map(|y| y.currency.to_string())
+                .collect::<Vec<String>>()
+        })
+        .collect();
+    let sell_for_price_rubs: Vec<i32> = items
+        .iter()
+        .flat_map(|x| x.sells.iter().map(|y| y.price_rub).collect::<Vec<i32>>())
+        .collect();
+    let sell_for_traders: Vec<String> = items
+        .iter()
+        .flat_map(|x| {
+            x.sells
+                .iter()
+                .map(|y| y.vendor.trader_name.to_string())
+                .collect::<Vec<String>>()
+        })
+        .collect();
+
+    let sell_for_sell_offer_fee_rate: Vec<i32> = items
+        .iter()
+        .flat_map(|x| {
+            x.sells
+                .iter()
+                .map(|y| y.vendor.sell_offer_fee_rate.unwrap_or(0))
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+    let sell_for_sell_requirement_fee_rate: Vec<i32> = items
+        .iter()
+        .flat_map(|x| {
+            x.sells
+                .iter()
+                .map(|y| y.vendor.sell_requirement_fee_rate.unwrap_or(0))
+                .collect::<Vec<i32>>()
+        })
+        .collect();
+
+    let sell_for_found_in_raid_required: Vec<bool> = items
+        .iter()
+        .flat_map(|x| {
+            x.sells
+                .iter()
+                .map(|y| y.vendor.found_in_raid_required.unwrap_or(false))
+                .collect::<Vec<bool>>()
+        })
+        .collect();
+
+    let sell_for_item_ids = items.iter().flat_map(|x| vec![x._id.to_string(); x.sells.len()]).collect::<Vec<String>>();
+
+    // SELLFOR BULK INSERT
+    sqlx::query!("INSERT INTO SellFor (price, currency, price_rub, trader_name, sell_offer_fee_rate, sell_requirement_fee_rate, found_in_raid_required, item_id) SELECT * FROM UNNEST($1::int[], $2::text[], $3::int[], $4::text[], $5::int[], $6::int[], $7::bool[], $8::text[]) ON CONFLICT (id) DO UPDATE SET price = EXCLUDED.price, currency = EXCLUDED.currency, price_rub = EXCLUDED.price_rub, trader_name = EXCLUDED.trader_name, sell_offer_fee_rate = EXCLUDED.sell_offer_fee_rate, sell_requirement_fee_rate = EXCLUDED.sell_requirement_fee_rate, found_in_raid_required = EXCLUDED.found_in_raid_required, item_id = EXCLUDED.item_id;",
+        &sell_for_prices,
+        &sell_for_currencys,
+        &sell_for_price_rubs,
+        &sell_for_traders,
+        &sell_for_sell_offer_fee_rate,
+        &sell_for_sell_requirement_fee_rate,
+        &sell_for_found_in_raid_required, 
+        &sell_for_item_ids).execute(&mut *txn).await?;
 
     txn.commit().await?;
     Ok(())
 }
 
+// no need to fully bulk optimize this since it is already fast
 async fn upsert_tasks(
     tasks: &Vec<Task>,
     pool: &sqlx::Pool<sqlx::Postgres>,
@@ -248,27 +321,23 @@ async fn upsert_tasks(
             .execute(&mut *txn)
             .await?;
 
-        for objective in task.objectives.iter() {
-            sqlx::query!(
-                "INSERT INTO Objective (obj_type, obj_description, map_name, map_wiki, task_id) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;",
-                &objective.obj_type,
-                &objective.obj_description,
-                &objective.maps.iter().map(|x| x.name.clone()).collect::<Vec<String>>().join(", "),
-                &objective.maps.iter().map(|x| x.wiki.clone()).collect::<Vec<String>>().join(", "),
-                &task._id,
-            ).execute(&mut *txn)
-            .await?;
-        }
+        sqlx::query!(
+            "INSERT INTO Objective (obj_type, obj_description, map_name, map_wiki, task_id) SELECT * FROM UNNEST($1::text[], $2::text[], $3::text[], $4::text[], $5::text[]) ON CONFLICT DO NOTHING;",
+            &task.objectives.iter().map(|x| x.obj_type.clone()).collect::<Vec<String>>(),
+            &task.objectives.iter().map(|x| x.obj_description.clone()).collect::<Vec<String>>(),
+            &task.objectives.iter().map(|x| x.maps.iter().map(|x| x.name.clone()).collect::<Vec<String>>().join(", ")).collect::<Vec<String>>(),
+            &task.objectives.iter().map(|x| x.maps.iter().map(|x| x.wiki.clone()).collect::<Vec<String>>().join(", ")).collect::<Vec<String>>(),
+            &vec![task._id.to_string();task.objectives.len()],
+        ).execute(&mut *txn)
+        .await?;
 
-        for task_requirement in task.task_requirements.iter() {
-            sqlx::query!(
-                "INSERT INTO TaskRequirement (status, req_task_id, task_id) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING;",
-                &task_requirement.status.join(", "),
-                &task_requirement.req_task_id.id,
-                &task._id,
-            ).execute(&mut *txn)
-            .await?;
-        }
+        sqlx::query!(
+            "INSERT INTO TaskRequirement (status, req_task_id, task_id) SELECT * FROM UNNEST($1::text[], $2::text[], $3::text[]) ON CONFLICT DO NOTHING;",
+            &task.task_requirements.iter().map(|x| x.status.join(", ")).collect::<Vec<String>>(),
+            &task.task_requirements.iter().map(|x| x.req_task_id.id.to_string()).collect::<Vec<String>>(),
+            &vec![task._id.to_string();task.task_requirements.len()],
+        ).execute(&mut *txn)
+        .await?;
     }
 
     txn.commit().await?;
