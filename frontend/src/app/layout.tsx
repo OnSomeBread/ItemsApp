de65dacaa4f63e711cf92f87e3fdcb5e736dba85
OnSomeBread <mark1qaz@comcast.net>
2025-09-23 12:18:29 -0500

    Umami analytics works properly now

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,10 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: { default: "Items", template: "%s" },
   description:
     "Escape From Tarkov Item and Task data insights. Task dependency graph displays all of the tasks that each trader provides along with their dependency direction",
-  srcipts: [
-    {
-      src: "https://cloud.umami.is/script.js",
-      dataWebsiteId: "99a9c2b0-6cb1-4714-b582-db3d5e8e0725",
-      strategy: "afterInteractive",
-    },
-  ],
 };
 
 export default function RootLayout({
@@ -22,6 +16,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div id="root">{children}</div>
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="99a9c2b0-6cb1-4714-b582-db3d5e8e0725"
+          strategy="afterInteractive"
+          defer={true}
+        />
       </body>
     </html>
   );
