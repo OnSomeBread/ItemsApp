de2c57ed86611a3eb2619a475b22bd55574a690f
OnSomeBread <mark1qaz@comcast.net>
2025-11-03 10:55:06 -0600

    added project code page to view source code

--- frontend/src/components/PageSwitch.tsx
+++ frontend/src/components/PageSwitch.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function PageSwitch() {
   return (
-    <nav className="mt-1 ml-4 w-140 justify-start">
+    <nav className="mt-1 ml-4 w-160 justify-start">
       <Link href="/items">Items</Link>
       <Link href="/items_compact">Items compact</Link>
       <Link href="/tasks">Tasks</Link>
@@ -10,6 +10,7 @@ function PageSwitch() {
       <Link href="/task_tree">Task Tree</Link>
       <Link href="/ammo">Ammo</Link>
       <Link href="/bitcoin">BitCoin</Link>
+      <a href="https://github.com/OnSomeBread/ItemsApp">Project Code</a>
       {/* <a onClick={() => window.history.back()}>Go Back</a> */}
     </nav>
   );
